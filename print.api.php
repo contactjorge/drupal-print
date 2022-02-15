ba6482bbb56f03bface0b267b287ba1b3741ed57
Jorge Lopez <jorge.lopez@elevatenp.org>
2022-02-15 15:21:02 -0600

    Rename inc

--- print.api.php
+++ print.api.php
@@ -15,26 +15,26 @@
  * - Printer-friendly pages
  *   - @link print.api.php API @endlink
  *   - @link print.module Module main file @endlink
- *   - @link print.pages.inc HTML generation @endlink
- *   - @link print.admin.inc Settings form @endlink
- *   - @link print.install (Un)Install routines @endlink
+ *   - @link print.pages.inc.php HTML generation @endlink
+ *   - @link print.admin.inc.php Settings form @endlink
+ *   - @link print.install.php (Un)Install routines @endlink
  *   - @link print.tpl.php Page generation template @endlink
- *   - @link print.views.inc Views integration @endlink
+ *   - @link print.views.inc.php Views integration @endlink
  *   - @link print_join_page_counter.inc Views join handler @endlink
  * - Send by email
  *   - @link print_mail.module Module main file @endlink
- *   - @link print_mail.inc Mail form and send mail routine @endlink
- *   - @link print_mail.admin.inc Settings form @endlink
+ *   - @link print_mail.inc.php Mail form and send mail routine @endlink
+ *   - @link print_mail.admin.inc.php Settings form @endlink
  *   - @link print_mail.install (Un)Install routines @endlink
- *   - @link print_mail.views.inc Views integration @endlink
+ *   - @link print_mail.views.php Views integration @endlink
  * - PDF version
  *   - @link print_pdf.api.php API @endlink
  *   - @link print_pdf.module Module main file @endlink
- *   - @link print_pdf.pages.inc PDF generation @endlink
- *   - @link print_pdf.admin.inc Settings form @endlink
+ *   - @link print_pdf.pages.php PDF generation @endlink
+ *   - @link print_pdf.admin.inc.php Settings form @endlink
  *   - @link print_pdf.install (Un)Install routines @endlink
- *   - @link print_pdf.drush.inc Drush commands @endlink
- *   - @link print_pdf.views.inc Views integration @endlink
+ *   - @link print_pdf.drush.inc.php Drush commands @endlink
+ *   - @link print_pdf.views.php Views integration @endlink
  *   - PDF library handlers:
  *     - dompdf
  *       - @link print_pdf_dompdf.module Module main file @endlink
@@ -62,16 +62,16 @@
  * - EPUB version
  *   - @link print_epub.api.php API @endlink
  *   - @link print_epub.module Module main file @endlink
- *   - @link print_epub.pages.inc EPUB generation @endlink
- *   - @link print_epub.admin.inc Settings form @endlink
- *   - @link print_epub.install (Un)Install routines @endlink
- *   - @link print_epub.drush.inc Drush commands @endlink
+ *   - @link print_epub.pages.inc.php EPUB generation @endlink
+ *   - @link print_epub.admin.php Settings form @endlink
+ *   - @link print_epub.install.php (Un)Install routines @endlink
+ *   - @link print_epub.drush.inc.php Drush commands @endlink
  *   - @link print_epub.views.inc Views integration @endlink
  *   - EPUB library handlers:
  *     - phpepub
  *       - @link print_epub_phpepub.module Module main file @endlink
- *       - @link print_epub_phpepub.pages.inc EPUB generation @endlink
- *       - @link print_epub_phpepub.drush.inc Drush commands @endlink
+ *       - @link print_epub_phpepub.pages.php EPUB generation @endlink
+ *       - @link print_epub_phpepub.drush.php Drush commands @endlink
  * - User Interface (Links)
  *   - @link print_ui.api.php API @endlink
  *   - @link print_ui.module Module main file @endlink
